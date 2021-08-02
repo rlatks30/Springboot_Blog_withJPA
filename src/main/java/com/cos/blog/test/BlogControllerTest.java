4225666db28b6a506e2325f91b5a64514862badf
산 <rlatks30@innerwave.co.kr>
2021-08-02 11:16:41 +0900

    환경설정 v1

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,13 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+//특정 어노테이션이 붙어있는 클래스 파일들을 new해서 (Ioc) 스프링 컨테이너에 관리해준다.
+@RestController
+public class BlogControllerTest {
+
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello spring boot</h1>";
+	}
+}
